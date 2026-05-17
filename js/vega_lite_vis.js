9da832c55c3af09de5c664624a3d5a2bb35fe005
ahas0067 <ahas0067@student.monash.edu>
2026-05-17 21:08:15 +1000

    Addition of the map chart and the bubble scatter plot

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,20 @@
 var vg_1 = "charts/chart_01_sport_ranking.vg.json";
+var vg_2 = "charts/chart_02_state_participation_map.vg.json";
+var vg_3 = "charts/chart_03_state_scatter.vg.json";
 
 vegaEmbed("#sport-ranking-chart", vg_1, {
+  actions: false
+}).then(function(result) {
+  // The Vega view is available as result.view
+}).catch(console.error);
+
+vegaEmbed("#state-participation-map", vg_2, {
+  actions: false
+}).then(function(result) {
+  // The Vega view is available as result.view
+}).catch(console.error);
+
+vegaEmbed("#state-scatter-chart", vg_3, {
   actions: false
 }).then(function(result) {
   // The Vega view is available as result.view
